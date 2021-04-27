41721df78b50f475821d673687b7354e350dd6a2
Rajkumar Patel <rajkumar.patel@seagate.com>
2021-04-27 09:04:15 +0530

    Fix commenting style

--- replicator/src/replicator.py
+++ replicator/src/replicator.py
@@ -1,41 +1,42 @@
 #!/usr/bin/env python3
 
-## @package replicator.py
+# s3 replicator server
 #
-#  Replicator script which host different end points
+# Replicator script which host different end points
 
 from aiohttp import web
 
 # Dictionary holding job_id and fdmi record
-jobs = {"job1": {"Bucket-Name": "testbucket"}}
+# e.g. : jobs = {"job1": {"obj_name": "foo"}}
+jobs = {}
 
 # List of active subscriber
 schedulers = []
 
 # List of jobs in progress
-jobs_inprogress = ['job1']
+jobs_inprogress = []
 
 # Route table declaration
 routes = web.RouteTableDef()
 
-## @list_jobs list jobs
-#
-#  Handler to list in-progress jobs
-
-
 @routes.get('/jobs')
 async def list_jobs(request):
+    """
+    list_jobs list jobs
+
+    Handler to list in-progress jobs
+    """
     print(jobs_inprogress)
     # Returns jobs_inprogress
     return web.Response(text="In-Progress jobs : {}".format(jobs_inprogress))
 
-## @get_job_attr get attributes
-#
-#  Handler to get job attributes
-
-
 @routes.get('/jobs/{job_id}')
 async def get_job_attr(request):
+    """
+    get_job_attr get attributes
+
+    Handler to get job attributes
+    """
     ID = (request.match_info['job_id'])
     print('ID is : {} '.format(ID))
     if ID in jobs.keys():
@@ -43,25 +44,25 @@ async def get_job_attr(request):
     else:
         return web.Response(text="ERROR : Job is not present!")
 
-## @add_jobs add jobs
-#
-#  Handler to add jobs to the queue
-
-
 @routes.put('/jobs')
 async def add_jobs(request):
+    """
+    add_jobs add jobs
+
+    Handler to add jobs to the queue
+    """
     entries = await request.json()
     print(entries)
     jobs.update(entries)
     return web.Response(text="Present jobs are : {}".format(jobs))
 
-## @abort_job abort job
-#
-#  Handler to abort a job with given job_id
-
-
 @routes.post('/jobs/{job_id}')
 async def abort_job(request):
+    """
+    abort_job abort job
+
+    Handler to abort a job with given job_id
+    """
     ID = (request.match_info['job_id'])
     print("ID is {}".format(ID))
     if ID in jobs.keys():
