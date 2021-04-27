74015933fc49bb55f5ce5e79f195686fa632deb2
Rajkumar Patel <rajkumar.patel@seagate.com>
2021-04-27 09:02:35 +0530

    Fix comment style

--- scheduler/src/scheduler.py
+++ scheduler/src/scheduler.py
@@ -1,32 +1,33 @@
 #!/usr/bin/env python3
 
-## @scheduler.py scheduler server
+# scheduler server - replicator manager
 #
-#  This file contains scheduler sever
-#  which exposes various REST endpoints
+# This file contains scheduler sever
+# which exposes various REST endpoints
 
 from aiohttp import web
 from urllib.parse import urlparse, parse_qs
 
-## Dictionary holding job_id and fdmi record
-jobs = {'jobA': {'K1': 'V1'}}  # dummy record is passed.
+# Dictionary holding job_id and fdmi record
+#  e.g. jobs = {'jobA': {'K1': 'V1'}}
+jobs = {}
 
-## List of active subscriber
-subscribers = ['sub1']
+# List of active subscriber
+subscribers = []
 
-## List of jobs in progress
-jobs_inprogress = ['jobA']
+# List of jobs in progress
+jobs_inprogress = []
 
 # Route table declaration
 routes = web.RouteTableDef()
 
-## @list_jobs Get in-progress jobs
-#
-#  Handler api to handle multiple query
-
-
 @routes.get('/jobs')
 async def list_jobs(request):
+    """
+    list_jobs Get in-progress jobs
+
+    Handler api to handle multiple query
+    """
     url = request.path_qs
     url_ob = urlparse(url)
     query = parse_qs(url_ob.query)
@@ -53,38 +54,38 @@ async def list_jobs(request):
     else:
         return web.Response(text='List of jobs : {}'.format(jobs.keys()))
 
-## @get_job_attr Get in progress jobs
-#
-#  Handler api to fetch job attributes
-
-
 @routes.get('/jobs/{job_id}')
 async def get_job_attr(request):
+    """
+    get_job_attr Get in progress jobs
+
+    Handler api to fetch job attributes
+    """
     ID = (request.match_info['job_id'])
     if ID in jobs.keys():
         return web.Response(text='attributes are : {}'.format(jobs['job_id']))
     else:
         return web.Response(text='ERROR : Job is not present!')
 
-## @add_jobs add jobs
-#
-#  Handler to add jobs to the job queue
-
-
 @routes.post('/jobs')
 async def add_jobs(request):
+    """
+    add_jobs add jobs
+
+    Handler to add jobs to the job queue
+    """
     entries = await request.json()
     print(entries)
     jobs.update(entries)
     print('Job list : {}'.format(jobs))
 
-## @add_subscriber add subscriber
-#
-#  Handler for Subscriber
-
-
 @routes.post('/subscribers')
 async def add_subscriber(request):
+    """
+    add_subscriber add subscriber
+
+    Handler for Subscriber
+    """
     sub_id = await request.json()
     print('subscriber is : {}'.format(sub_id))
     if sub_id in subscribers:
@@ -94,22 +95,22 @@ async def add_subscriber(request):
         print(subscribers)
         return web.Response(text='Replicator added! : {}'.format(subscribers))
 
-## @list_subscribers List subscriber
-#
-#  Handler to list subscribers
-
-
 @routes.get('/subscribers')
 async def list_subscribers(request):
+    """
+    list_subscribers List subscriber
+
+    Handler to list subscribers
+    """
     return web.Response(text='subscribers are : {} '.format(subscribers))
-
-## update_job_attr update job attributes
-#
-#  Update attributes for job_id
-
 
 @routes.put('/jobs/{job_id}')
 async def update_job_attr(request):
+    """
+    update_job_attr update job attributes
+
+    Update attributes for job_id
+    """
     val = await request.json()  # ToDo
     ID = (request.match_info['job_id'])
     print('ID is {}'.format(ID))
