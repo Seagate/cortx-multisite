2a6e7f924391678f55e124809ab4fc794946d6e5
Rajkumar Patel <rajkumar.patel@seagate.com>
2021-08-12 02:57:30 -0600

    Fix review comments
    Signed-off-by: Rajkumar Patel <rajkumar.patel@seagate.com>

--- s3/replication/README.md
+++ s3/replication/README.md
@@ -29,10 +29,10 @@ Run basic tests for each module. Clean, Build and test.
 make test
 ```
 
-## Jenkins build & test script
-Jenkins script to create virtual environment, build all modules and also starts s3-in-memory server, s3replicationmanager and s3replicator as systemd proccesses and then run load transfer tests for s3replicationmanager and s3replicator.
+## Build & test script
+A script which facilitates users to create virtual environment, build all modules and also starts s3-in-memory server, s3replicationmanager and s3replicator as systemd proccesses and then run load transfer tests for s3replicationmanager and s3replicator. This script will also be used by jenkins to build and run tests.
 ```sh
-sh build-runalltest.sh
+sh build_and_test.sh
 ```
 
 ## Design documents
--- s3/replication/build_and_test.sh
+++ s3/replication/build_and_test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 #
 # Copyright (c) 2021 Seagate Technology LLC and/or its Affiliates
 #
@@ -22,12 +21,11 @@
 # build-runalltest.sh allows to build all multisite moudles,
 # create virtual environment, populating service files with
 # correct paths, copying to default systemd location and
-# starting daemon processes and running load transfer tests for
+# starting background processes and running load transfer tests for
 # s3replicator and s3replicationmanager.
 #
-# Usage : sh ./build-runalltest.sh
+# Usage : sh ./build_and_test.sh
 # 
-
 
 # Project root directory
 MULTISITE_ROOT_DIR="$(cd ../../; pwd)"
@@ -36,6 +34,9 @@ echo "Project directory : "$MULTISITE_ROOT_DIR
 # Replication directory
 REPLICATION_DIR="$(pwd)"
 echo "Replication directory :" $REPLICATION_DIR
+
+REPLICATION_VENV=$REPLICATION_DIR/s3env/bin
+echo "Virtual environment directory : "$REPLICATION_VENV
 
 # Module directory
 S3_REPLICATION_COMMON_SRC_DIR=$REPLICATION_DIR/common/src
@@ -52,6 +53,24 @@ echo "Common module directory :" $S3_REPLICATION_COMMON_SRC_DIR
 echo "Manager module directory :" $S3_REPLICATION_MANAGER_SRC_DIR
 echo "Replicator module directory :" $S3_REPLICATOR_SRC_DIR
 
+# Helper method to stop background services.
+stop_replication_services()
+{
+    systemctl stop s3_in_memory.service
+    systemctl stop s3replicationmanager.service
+    systemctl stop s3replicator.service
+
+    # Check if any service name is passed or not.
+    # If passed then it's an error case. Print an error message and exit.
+    if [ $# -eq 0 ]; then
+        echo "Shut down all processes!"
+    else
+        FAILED_SVC=$1
+        echo "ERROR : $FAILED_SVC service activation failed!"
+        exit -1
+    fi
+}
+
 # Active an environment
 python3 -m venv $REPLICATION_DIR/s3env
 source $REPLICATION_DIR/s3env/bin/activate
@@ -59,12 +78,8 @@ source $REPLICATION_DIR/s3env/bin/activate
 # Check and download dependencies
 pip3 install --upgrade setuptools
 
-pip3 install -r $REPLICATION_DIR/common/requirements.txt
-pip3 install -r $REPLICATION_DIR/manager/requirements.txt
-pip3 install -r $REPLICATION_DIR/replicator/requirements.txt
-
 cd $REPLICATION_DIR
-make clean install
+make install
 
 # Create and copy files
 S3_IN_MEMRORY_SVC_FILE=$S3_REPLICATION_COMMON_SRC_DIR/systemd/s3_in_memory.service
@@ -89,6 +104,7 @@ else
         mkdir $CORTX_S3_DIR
     fi
     touch $CORTX_S3_CRED_FILE
+    # Placeholder for keys to run against in-memory s3 server.
     echo "access_key: '<your access_key>'" > $CORTX_S3_CRED_FILE
     echo "secret_key: '<your secret_key>'" >> $CORTX_S3_CRED_FILE
 fi
@@ -98,92 +114,82 @@ rm -f $SYSD_REPLICATION_MANAGER_SVC_FILE
 rm -f $SYSD_REPLICATOR_SVC_FILE
 
 # Strip hardcoded lines
-head -n -2 $S3_IN_MEMRORY_SVC_FILE > $SYSD_S3_IN_MEMORY_SVC_FILE
-head -n -2 $MANGER_SVC_FILE > $SYSD_REPLICATION_MANAGER_SVC_FILE
-head -n -2 $REPLICATOR_SVC_FILE > $SYSD_REPLICATOR_SVC_FILE
+head -n -1 $S3_IN_MEMRORY_SVC_FILE > $SYSD_S3_IN_MEMORY_SVC_FILE
+head -n -1 $MANGER_SVC_FILE > $SYSD_REPLICATION_MANAGER_SVC_FILE
+head -n -1 $REPLICATOR_SVC_FILE > $SYSD_REPLICATOR_SVC_FILE
 
-# Setting environment and working directory in service files
-Environment="PYTHONPATH=$S3_REPLICATION_COMMON_SRC_DIR"
-echo "Environment=$Environment" >> $SYSD_S3_IN_MEMORY_SVC_FILE
-echo "Environment=$Environment" >> $SYSD_REPLICATION_MANAGER_SVC_FILE
-echo "Environment=$Environment" >> $SYSD_REPLICATOR_SVC_FILE
+echo "ExecStart=$REPLICATION_VENV/python3 $S3_REPLICATION_COMMON_TEST_DIR/s3_in_memory.py" >> $SYSD_S3_IN_MEMORY_SVC_FILE
+echo "ExecStart=$REPLICATION_VENV/python3 -m s3replicationmanager" >> $SYSD_REPLICATION_MANAGER_SVC_FILE
+echo "ExecStart=$REPLICATION_VENV/python3 -m s3replicator" >> $SYSD_REPLICATOR_SVC_FILE
 
-echo "WorkingDirectory=$S3_REPLICATION_COMMON_SRC_DIR" >> $SYSD_S3_IN_MEMORY_SVC_FILE
-echo "ExecStart=/usr/bin/python3 $S3_REPLICATION_COMMON_TEST_DIR/s3_in_memory.py" >> $SYSD_S3_IN_MEMORY_SVC_FILE
-
-echo "WorkingDirectory=$S3_REPLICATION_MANAGER_SRC_DIR" >> $SYSD_REPLICATION_MANAGER_SVC_FILE
-echo "WorkingDirectory=$S3_REPLICATOR_SRC_DIR" >> $SYSD_REPLICATOR_SVC_FILE
-
-# Start daemon process
+# Start background process
 DAEMON_SVC_DIR=/etc/systemd/system 
 
 echo "Service files created at : "$DAEMON_SVC_DIR
 
 sudo systemctl daemon-reload
 
-systemctl enable s3_in_memory.service
-systemctl enable s3replicationmanager.service
-systemctl enable s3replicator.service
-
-# Starting services
+# Starting s3_in_memory service 
 systemctl start s3_in_memory.service
 svc_status=$(systemctl is-active s3_in_memory.service)
-
 if [ $svc_status != "active" ]
 then
-    {echo "ERROR : s3_in_memory service activation failed!"; exit -1}
-else
-    echo "s3_in_memory service is actived"
-    
-    systemctl start s3replicationmanager.service
-    svc_status=$(systemctl is-active s3replicationmanager.service)
+    stop_replication_services "s3_in_memory"
+fi
 
-    if [ $svc_status != "active" ]
-    then
-        $(systemctl stop s3_in_memory.service)
-        {echo "ERROR : s3replicationmanager service activation failed!"; exit -1}
-    else
-        echo "s3replicationmanager service is actived"
-        
-        systemctl start s3replicator.service
-        svc_status=$(systemctl is-active s3replicator.service)
+# Starting s3replicationmanager service 
+systemctl start s3replicationmanager.service
+svc_status=$(systemctl is-active s3replicationmanager.service)
+if [ $svc_status != "active" ]
+then
+    stop_replication_services "s3replicationmanager"
+fi
 
-        if [ $svc_status != "active" ]
-        then
-            $(systemctl stop s3_in_memory.service)
-            $(systemctl stop s3replicationmanager.service)
-            {echo "ERROR : s3replicator service activation failed!"; exit -1}
-        else
-            echo "s3replicator service is actived"
-        fi
-    fi
+# Starting s3replictor service 
+systemctl start s3replicator.service
+svc_status=$(systemctl is-active s3replicator.service)
+if [ $svc_status != "active" ]
+then
+    stop_replication_services "s3replicator"
 fi
 
 # Start load transfer tests
 echo "----------------- s3replicationmanager test started -------------------"
 sleep 2 # For better logging
-python3 $S3_REPLICATION_MANAGER_TEST_DIR/load_transfer_test.py
+$REPLICATION_VENV/python3 $S3_REPLICATION_MANAGER_TEST_DIR/load_transfer_test.py
+if [[ $? = 0 ]]; then
+    echo "success"
+    MANAGER_LOAD_TRANSFER_TEST_RESULT="PASSED"
+else
+    echo "failure: $?"
+    MANAGER_LOAD_TRANSFER_TEST_RESULT="FAILED"
+fi
 echo "------------------ s3replicationmanager test ended --------------------"
 
 echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
 
 echo "---------------------- s3replicator test started ----------------------"
 sleep 2
-python3 $S3_REPLICATOR_TEST_DIR/load_transfer_test.py
+$REPLICATION_VENV/python3 $S3_REPLICATOR_TEST_DIR/load_transfer_test.py
+if [[ $? = 0 ]]; then
+    echo "success"
+    REPLICATOR_LOAD_TRANSFER_TEST_RESULT="PASSED"
+else
+    echo "failure: $?"
+    REPLICATOR_LOAD_TRANSFER_TEST_RESULT="FAILED"
+fi
 echo "---------------------- s3replicator test ended ------------------------"
 
-# Stopping all the started daemon processes
+# Stopping all the started background processes
 echo "Shutting down all started processes..."
+stop_replication_services
 sleep 2
 
-systemctl stop s3_in_memory.service
-systemctl stop s3replicationmanager.service
-systemctl stop s3replicator.service
-
-# Clean the build
-cd $REPLICATION_DIR
-echo "Cleaning up the build..."
-make clean
+# Test Summary 
+echo "========================= Test Summary ==========================="
+echo "Test 1: Load transfer for s3replicationmaanger [$MANAGER_LOAD_TRANSFER_TEST_RESULT]"
+echo "Test 2: Load transfer for s3replicatior [$REPLICATOR_LOAD_TRANSFER_TEST_RESULT]"
+echo "=================================================================="
 
 # Exit the environment
 deactivate
